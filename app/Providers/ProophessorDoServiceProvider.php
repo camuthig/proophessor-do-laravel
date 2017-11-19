12efc1b1f1bf2e0fa143446375161963f4c40d99
Chris Muthig <camuthig@gmail.com>
2017-11-19 10:01:49 -0800

    Setup a Doctrine Connection

--- app/Providers/ProophessorDoServiceProvider.php
+++ app/Providers/ProophessorDoServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
 use Illuminate\Support\ServiceProvider;
 use Prooph\ProophessorDo\Infrastructure\Service\ChecksUniqueUsersEmailAddressFromReadModel;
 use Prooph\ProophessorDo\Model\User\Service\ChecksUniqueUsersEmailAddress;
@@ -13,5 +15,26 @@ class ProophessorDoServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(ChecksUniqueUsersEmailAddress::class, ChecksUniqueUsersEmailAddressFromReadModel::class);
+
+        $this->app->singleton(Connection::class, function () {
+            $default = config('database.default');
+
+            switch ($default) {
+                case 'mysql':
+                    $driver = 'pdo_mysql';
+                    break;
+                case 'postgres':
+                    $driver = 'pdo_pgsql';
+            }
+
+            return DriverManager::getConnection([
+                'dbname'   => config(sprintf('database.%s.database'), $default),
+                'user'     => config(sprintf('database.%s.username'), $default),
+                'password' => config(sprintf('database.%s.password'), $default),
+                'host'     => config(sprintf('database.%s.host'), $default),
+                'port'     => config(sprintf('database.%s.port'), $default),
+                'driver'   => $driver,
+            ]);
+        });
     }
 }
